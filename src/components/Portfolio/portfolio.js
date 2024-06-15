ad7f3e4240d9b00991d011d053bbf67e6a3a9116
Riadh Ibrahim <110717872+Riadh-Ibrahim@users.noreply.github.com>
2024-06-15 08:44:59 +0100

    Update portfolio.js

--- src/components/Portfolio/portfolio.js
+++ src/components/Portfolio/portfolio.js
@@ -1,75 +1,67 @@
-.projects{
-    background-color: black;
-    text-align: center;
-}
-
-.projects .content {
-  margin-top: 30px;  
-}
-
-.project-card{
-    background-color: #fff;
-    border: 1px splid #fff;
-    min-height: 22em;
-    width: 33rem;
-    overflow: hidden;
-    border-radius: 10px;
-    margin: 20px;
-    transition: 0.4s ease;
-}
-
-.project-card:hover {
-    transform: scale(1.1);
-}
-
-.project-image img{
-    width: 100%;
-}
-
-.project-card:hover .project-image {
-   opacity: 0.7;
-}
-
-.project-info {
-    padding: 1em;
-}
-
-.project-category {
-    font-size: 0.9em;
-    color: #000000ec;
-}
-
-.project-title {
-    display: flex;
-    justify-content: space-between;
-    text-transform: uppercase;
-    font-weight: 800;
-    margin-top: 10px;
-    color: #6b14149a;
-}
-
-#code {
-    font-size: 0.5em;
-}
-
-.more-details{
-    text-decoration: none;
-    color: #3a6cf4;
-}
-
-.more-details:hover {
-    color: #601cfc;
-}
-
-@media screen and (max-width: 680px) {
-    .project-card{
-        width: 25rem;
-    }
-}
+import React from 'react';
+import './portfolio.css';
+import kyufiImg from '../../assets/kyufi.png'
+import entreprenetImg from '../../assets/entreprenet.png'
+import todoImg from '../../assets/todo.png'
+import hiddenImg from '../../assets/number.png'
 
 
-@media screen and (max-width: 480px) {
-    .project-card{
-        width: 22rem;
-    }
-}
+const Portfolio = () => {
+    return (
+        <section class="projects" id="projects">
+                <h2 class="title">Projects :</h2>
+                <div class="content">
+                    <div class="project-card">
+                        <div class="project-image">
+                            <img src={entreprenetImg} alt='' />
+                        </div>
+                        <div class="project-info">
+                            <p class="project-category">A platform buit with MERN stack that allows investors to connect with entrepreneurs in a dynamic environment</p>
+                            <strong class="project-title">
+                                <span><h3>EntrepreNet</h3></span>
+                                <a href="https://github.com/Riadh-Ibrahim/Entreprenet" class="more-details">View More</a>
+                            </strong>
+                        </div>
+                    </div>
+                    <div class="project-card">
+                        <div class="project-image">
+                            <img src={kyufiImg} alt=''/>
+                        </div>
+                        <div class="project-info">
+                            <p class="project-category">An <b>e-commerce</b> web application designed for a <b>Coffee Shop</b>, facilitating customers to place orders seamlessly, built with HTML, CSS, JavaScript, PHP, and MySQL.</p>
+                            <strong class="project-title">
+                                <span><b><h3>Kyufi Coffee Shop</h3></b></span>
+                                <a href="https://github.com/Riadh-Ibrahim/Coffee-Shop-Website" class="more-details">View More</a>
+                            </strong>
+                        </div>
+                    </div>
+                    <div class="project-card">
+                        <div class="project-image">
+                            <img src={todoImg} alt=''/>
+                        </div>
+                        <div class="project-info">
+                            <p class="project-category">A <b>Todo List</b> page using HTML, CSS & JavaScript</p>
+                            <strong class="project-title">
+                                <span><h3>TODO List</h3></span>
+                                <a href="https://github.com/Riadh-Ibrahim/Todo-List" class="more-details">View More</a>
+                            </strong>
+                        </div>
+                    </div>
+                    <div class="project-card">
+                    <div class="project-image">
+                        <img src={hiddenImg} alt=''/>
+                    </div>
+                        <div class="project-info">
+                            <p class="project-category">A web page that simulates the game of <b>Hiddem Number</b> using HTML, CSS & JavaScript</p>
+                            <strong class="project-title">
+                                <span><h3>Hidden Number</h3></span>
+                                <a href="https://github.com/Riadh-Ibrahim/Jeu-du-chiffre-cach-" class="more-details">View More</a>
+                            </strong>
+                        </div>
+                    </div>
+                </div>
+        </section>
+    );
+};
+
+export default Portfolio;
